a9fe9c4253382dcf2fd0be9a070d5fea57e32e7e
taikoubou <5717958+taikoubou@users.noreply.github.com>
2023-06-12 02:18:57 +0900

    Stop Section2

--- section2/src/main.rs
+++ section2/src/main.rs
@@ -1,3 +1,22 @@
+extern crate pnet;
+
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
+
+use std::env;
+
+mod packets;
+use packets::GettableEndPoints;
+
+const WIDTH:usize = 20;
+
 fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
